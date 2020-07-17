fed2b0ce952a4290b02e56ba336eef2319c18163
bogdanaks <bogdanaks@bk.ru>
2020-07-17 22:41:31 +0300

    Change active class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,8 +5,8 @@
                 <div class="col s6 offset-s3">
                     <router-link to='/' class="brand-logo">Todo-Vue</router-link>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <router-link to="/" active-class="active" tag="li"><a href="#">Create</a></router-link>
-                        <router-link to="/List" tag="li"><a href="#">List</a></router-link>
+                        <router-link to="/" active-class="active" tag="li" exact><a>Create</a></router-link>
+                        <router-link to="/list" active-class="active" tag="li" exact><a>List</a></router-link>
                     </ul>
                 </div>
             </div>
