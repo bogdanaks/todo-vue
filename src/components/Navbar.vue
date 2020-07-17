c31fd84ef85d7b873f281518b129cb9a2dbe343f
bogdanaks <bogdanaks@bk.ru>
2020-07-17 22:40:00 +0300

    Change logo a-href to router-link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <div class="nav-wrapper container">
             <div class="row">
                 <div class="col s6 offset-s3">
-                    <a href="#" class="brand-logo">Todo-Vue</a>
+                    <router-link to='/' class="brand-logo">Todo-Vue</router-link>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <router-link to="/" active-class="active" tag="li"><a href="#">Create</a></router-link>
                         <router-link to="/List" tag="li"><a href="#">List</a></router-link>
