c495f43456b48b877bf6500f2df477c7b5ee74b6
bogdanaks <bogdanaks@bk.ru>
2020-07-17 12:02:37 +0300

    Import nav component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Navbar />
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: { Navbar }
+}
+</script>
 
 <style>
 #app {
