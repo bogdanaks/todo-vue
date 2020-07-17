893bc6cb5d341bc1e982f79510cd0b65b9435c2e
bogdanaks <bogdanaks@bk.ru>
2020-07-17 11:33:46 +0300

    Update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <template>
   <nav>
-      
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Todo-Vue</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Create</a></li>
+                <li><a href="#">List</a></li>
+            </ul>
+        </div>
   </nav>
 </template>
 
