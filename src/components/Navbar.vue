5457e8aa8c07375b9709caf18e00946e120f6879
bogdanaks <bogdanaks@bk.ru>
2020-07-17 11:57:52 +0300

    Update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,17 @@
 <template>
-  <nav>
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">Todo-Vue</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#">Create</a></li>
-                <li><a href="#">List</a></li>
-            </ul>
+    <nav class="blue darken-1">
+        <div class="nav-wrapper container">
+            <div class="row">
+                <div class="col s6 offset-s3">
+                    <a href="#" class="brand-logo">Todo-Vue</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <router-link to="/" active-class="active" tag="li"><a href="#">Create</a></router-link>
+                        <router-link to="/List" tag="li"><a href="#">List</a></router-link>
+                    </ul>
+                </div>
+            </div>
         </div>
-  </nav>
+    </nav>
 </template>
 
 <script>
